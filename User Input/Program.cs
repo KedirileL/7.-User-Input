f0323c353e2560722540e20437c6388862e9ef76
user <user@LAPTOP-KAN0NU4I>
2023-09-14 06:34:51 +0200

    Add project files.

---
+++ User Input/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\nHi " + name);
+
+            Console.WriteLine("\nEnter you age: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("\nYou are " + age + " years.");
+
+            //Freeze the console
+            Console.ReadLine();
+        }
+    }
+}
